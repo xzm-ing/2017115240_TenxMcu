143eb418af2414fcf5dd239cb15ef95d7fd4e056
PC-201709222117\Administrator <2631742051@qq.com>
2019-12-01 00:38:08 +0800

    led单端口按键控灯实现

---
+++ prj_led4-onePort/code(78Bsop32)/keys.c
@@ -0,0 +1,14 @@
+#define __keys_c
+#include "includeAll.h"
+//=============================================================================
+void GetKeys() {
+  static uint8_t tempKeyValue = D_keyNull;
+//   Delay(5);
+
+  if (P_key1 == 0) {
+    tempKeyValue = D_keyValue1;
+  } else {
+    keyValue = tempKeyValue;
+    tempKeyValue = D_keyNull;
+  }
+}
