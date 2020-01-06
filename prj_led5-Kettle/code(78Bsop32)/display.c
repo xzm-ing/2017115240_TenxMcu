fd4ce7dee4064b7ab83ffaeaae1e83b6e5741ce7
PC-201709222117\Administrator <2631742051@qq.com>
2020-01-06 10:31:55 +0800

    热水壶实验——数码管显示模块

--- prj_led5-Kettle/code(78Bsop32)/display.c
+++ prj_led5-Kettle/code(78Bsop32)/display.c
@@ -34,3 +34,225 @@ void LED2_ON() {
 		P_led1=0;
 		P_led2=1;
 }
+
+
+/**********************************
+SMG_COM1控制左数码管
+SMG_COM2控制右数码管
+其他管脚分别控制相应位置led的亮灭
+**********************************/
+//数码管初始化
+void smg_init() {
+		//初始化各个灯脚，赋值为0
+		SMG_A=0;
+		SMG_B=0;
+		SMG_C=0;
+		SMG_D=0;
+		SMG_E=0;
+		SMG_F=0;
+		SMG_G=0;
+		SMG_DG=0;
+	
+		F_ledcom_Off();
+}
+
+//数码管选择
+void smg_choice(uint8_t smgx) { 
+    //选择左数码管
+		if(smgx == SMG_L)         
+    {
+        SMG_COM1 = 0;
+        SMG_COM2 = 1;
+    }
+		//选择右数码管
+    if(smgx == SMG_R)
+    {
+        SMG_COM1 = 1;
+        SMG_COM2 = 0;
+    }
+}
+
+//选择数码管并显示数字0-9
+void smg_display_num(uint8_t smgx,int num) {
+		//选择左数码管
+		if(smgx == SMG_L)         
+    {
+				smg_choice(1);
+    }
+		//选择右数码管
+    if(smgx == SMG_R)
+    {
+				smg_choice(2);
+    }
+		
+		switch(num)
+		{
+			case 0:SMG_A = 1;
+						 SMG_B = 1;
+						 SMG_C = 1;
+						 SMG_D = 1;
+						 SMG_E = 1;
+						 SMG_F = 1;
+						 SMG_G = 0;
+						 SMG_DG = 1;
+						 break;
+			
+			case 1:SMG_A = 0;
+						 SMG_B = 1;
+						 SMG_C = 1;
+						 SMG_D = 0;
+						 SMG_E = 0;
+						 SMG_F = 0;
+						 SMG_G = 0;
+						 SMG_DG = 1;
+						 break;
+			
+			case 2:SMG_A = 1;
+						 SMG_B = 1;
+						 SMG_C = 0;
+						 SMG_D = 1;
+						 SMG_E = 1;
+						 SMG_F = 0;
+						 SMG_G = 1;
+						 SMG_DG = 1;
+						 break;
+
+			case 3:SMG_A = 1;
+						 SMG_B = 1;
+						 SMG_C = 1;
+						 SMG_D = 1;
+						 SMG_E = 0;
+						 SMG_F = 0;
+						 SMG_G = 1;
+						 SMG_DG = 1;
+						 break;
+
+			case 4:SMG_A = 0;
+						 SMG_B = 1;
+						 SMG_C = 1;
+						 SMG_D = 0;
+						 SMG_E = 0;
+						 SMG_F = 1;
+						 SMG_G = 1;
+						 SMG_DG = 1;
+						 break;
+			case 5:SMG_A = 1;
+						 SMG_B = 0;
+						 SMG_C = 1;
+						 SMG_D = 1;
+						 SMG_E = 0;
+						 SMG_F = 1;
+						 SMG_G = 1;
+						 SMG_DG = 1;
+						 break;
+
+			case 6:SMG_A = 1;
+						 SMG_B = 0;
+						 SMG_C = 1;
+						 SMG_D = 1;
+						 SMG_E = 1;
+						 SMG_F = 1;
+						 SMG_G = 1;
+						 SMG_DG = 1;
+						 break;
+						 
+			case 7:SMG_A = 1;
+						 SMG_B = 1;
+						 SMG_C = 1;
+						 SMG_D = 0;
+						 SMG_E = 0;
+						 SMG_F = 0;
+						 SMG_G = 0;
+						 SMG_DG = 1;
+						 break;
+			
+			case 8:SMG_A = 1;
+						 SMG_B = 1;
+						 SMG_C = 1;
+						 SMG_D = 1;
+						 SMG_E = 1;
+						 SMG_F = 1;
+						 SMG_G = 1;
+						 SMG_DG = 1;
+						 break;
+				
+			case 9:SMG_A = 1;
+						 SMG_B = 1;
+						 SMG_C = 1;
+						 SMG_D = 1;
+						 SMG_E = 0;
+						 SMG_F = 1;
+						 SMG_G = 1;
+						 SMG_DG = 1;
+						 break;
+		}
+}
+
+
+//数码管显示on(开)和of(关)
+void smg_display_num(uint8_t On_or_Of) {
+		if(On_or_Of == SMG_ON){
+				smg_choice(1);
+				SMG_A = 1;
+				SMG_B = 1;
+				SMG_C = 1;
+				SMG_D = 1;
+				SMG_E = 1;
+				SMG_F = 1;
+				SMG_G = 0;
+				SMG_DG = 0;
+	
+				smg_choice(2);
+				SMG_A = 1;
+			  SMG_B = 1;
+				SMG_C = 1;
+				SMG_D = 0;
+				SMG_E = 1;
+				SMG_F = 1;
+				SMG_G = 0;
+				SMG_DG = 0;
+		}
+		else if(On_or_Of == SMG_OFF){
+				smg_choice(1);
+				SMG_A = 1;
+				SMG_B = 1;
+				SMG_C = 1;
+				SMG_D = 1;
+				SMG_E = 1;
+				SMG_F = 1;
+				SMG_G = 0;
+				SMG_DG = 0;
+	
+				smg_choice(2);
+				SMG_A = 1;
+			  SMG_B = 0;
+				SMG_C = 0;
+				SMG_D = 0;
+				SMG_E = 1;
+				SMG_F = 1;
+				SMG_G = 1;
+				SMG_DG = 0;
+		}
+		else
+		{
+				smg_choice(1);
+				SMG_A = 1;
+			  SMG_B = 1;
+				SMG_C = 1;
+				SMG_D = 0;
+				SMG_E = 1;
+				SMG_F = 1;
+				SMG_G = 0;
+				SMG_DG = 0;
+	
+				smg_choice(2);
+				SMG_A = 1;
+				SMG_B = 1;
+				SMG_C = 1;
+				SMG_D = 1;
+				SMG_E = 1;
+				SMG_F = 1;
+				SMG_G = 0;
+				SMG_DG = 0;
+		}
+}
--- prj_led5-Kettle/code(78Bsop32)/display.h
+++ prj_led5-Kettle/code(78Bsop32)/display.h
@@ -1,13 +1,32 @@
 #ifndef __display_h
 #define __display_h
 // Hal: exp: #define P_ledcom P10 -----------------
+//定义LED灯IO口
 #define P_ledcom P1_0 
 #define P_led1 P1_4
 #define P_led2 P1_2
 
+//定义数码管控制IO口
+#define SMG_COM1 P1_1		//控制左数码管
+#define SMG_COM2 P1_6		//控制右数码管
+
+//定义数码管八个led的IO口
+#define SMG_A P1_4
+#define SMG_B P3_4
+#define SMG_C P3_5
+#define SMG_D P1_7
+#define SMG_E P3_6
+#define SMG_F P1_2
+#define SMG_G P3_2
+#define SMG_DG P1_3
+
 // Const: exp: #define D_data 1 ----------------
 #define D_ledcom_Off 1  //高电平灭
 #define D_ledcom_On 0 	//低电平亮
+
+//选择左、右数码管
+#define SMG_L 1
+#define SMG_R 2
 // Globle Var -----------------------------------------
 #ifdef __display_c
 bit bflag0;
@@ -22,7 +41,15 @@ extern bit bflag;
 #define F_ledcom_Off() P_ledcom = D_ledcom_Off     
 #define F_ledcom_Neg() P_ledcom = ~P_ledcom
 // Function ------------------------------------
+//LED灯
 void LED1_and_LED2();
 void LED1_ON();
 void LED2_ON();
+
+//数码管
+void smg_init();
+void smg_choice(uint8_t smgx);
+void smg_display_num(uint8_t smgx,int num);
+void smg_display_num(uint8_t On_or_Of);
+
 #endif
